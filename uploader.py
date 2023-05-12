832fa06d56fefea664eafd28dfcfe02caa74734e
Jort <jort.developer@gmail.com>
2023-05-12 16:36:13 +0200

    update program for personal settings

--- uploader.py
+++ uploader.py
@@ -28,13 +28,25 @@ def printt(*argss, **kwargs):
         print(to_print, **kwargs)
 
 
+def warn(*argss, **kwargs):
+    to_print = " ".join(map(str, argss))
+    to_print = "WARNING: " + to_print
+    print(to_print, **kwargs)
+
+
+# SELENIUM UTIL
+
+########################################################################################################################
 def select_dropdown(xpath, option_text):
-    dropdown = driver.find_element(By.XPATH, xpath)
-    center_element(dropdown)
-    time.sleep(delay)
-    select = Select(dropdown)
-    select.select_by_visible_text(option_text)
-    time.sleep(delay)
+    try:
+        dropdown = driver.find_element(By.XPATH, xpath)
+        center_element(dropdown)
+        time.sleep(delay)
+        select = Select(dropdown)
+        select.select_by_visible_text(option_text)
+        time.sleep(delay)
+    except Exception:
+        warn(f"Could not select option '{option_text}'")
 
 
 def sleep_until_url_change():
@@ -46,7 +58,7 @@ def sleep_until_url_change():
             time.sleep(1)
     except:
         traceback.print_exc()
-        print(f"URL not changed but breaking because of above exception")
+        warn(f"URL not changed but breaking because of above exception")
 
 
 # only support for 2 tabs: 1 and 2
@@ -86,17 +98,25 @@ def tab_and_get(url: str, tab_number: int):
 
 def remove_hidden_attribute(xpath):
     printt(f"Removing hidden tag on element with xpath={xpath}")
-    element = driver.find_element(By.XPATH, xpath)
-    driver.execute_script(
-        "arguments[0].removeAttribute('hidden')", element)
+    try:
+        element = driver.find_element(By.XPATH, xpath)
+        driver.execute_script(
+            "arguments[0].removeAttribute('hidden')", element)
+    except Exception:
+        warn(f"Could not remove hidden tag.")
 
 
 def center_element(element):
-    driver.execute_script(
-        "arguments[0].scrollIntoView({'block':'center','inline':'center'})", element)
+    printt(f"Centering element: {element}")
+    try:
+        driver.execute_script(
+            "arguments[0].scrollIntoView({'block':'center','inline':'center'})", element)
+    except Exception:
+        warn(f"Failed to center element {element}")
 
 
 def enter_field(xpath, text):
+    printt(f"Entering text in field: '{text}'")
     try:
         element = WebDriverWait(driver, timeout).until(
             EC.presence_of_element_located((By.XPATH, xpath)))
@@ -104,30 +124,30 @@ def enter_field(xpath, text):
             quit(f"Could not find element with {xpath} to enter {text}")
         center_element(element)
         time.sleep(delay)
-        printt(f"Entering text in field: '{text}'")
         element.send_keys(text)
         time.sleep(delay)
-    except Exception as e:
-        traceback.print_exc()
-        raise e
+    except Exception:
+        warn(f"Failed to enter '{text}'")
 
 
 def click_button(xpath):
+    printt(f"Clicking button: {xpath}")
     try:
         button = WebDriverWait(driver, timeout).until(
             EC.presence_of_element_located((By.XPATH, xpath)))
-        # element = driver.find_element(by=By.XPATH, value=xpath)
-        if button is None:
-            quit(f"Could not find button with {xpath} to click")
         center_element(button)
         time.sleep(delay)
-        printt(f"Clicking button with xpath={xpath}")
         button.click()
         time.sleep(delay)
-    except Exception as e:
-        traceback.print_exc()
-        raise e
+    except Exception:
+        warn(f"Could not click button.")
 
+
+########################################################################################################################
+# SELENIUM UTIL
+
+# MARKTPLAATS
+########################################################################################################################
 
 def login_marktplaats(username, password):
     printt(f"Logging in to Marktplaats with {username}, {password}")
@@ -144,20 +164,6 @@ def login_marktplaats(username, password):
     time.sleep(2)
 
     printt(f"Marktplaats logged in")
-
-
-def login_tweakers(username, password):
-    printt(f"Logging in to Tweakers with {username}, {password}")
-    tab_and_get(tweakers_login_url, 2)
-    username_xpath = '//*[@id="tweakers_login_form_user"]'
-    password_xpath = '//*[@id="tweakers_login_form_password"]'
-    enter_field(username_xpath, username)
-    enter_field(password_xpath, password)
-    time.sleep(delay)
-    password_field = driver.find_element(By.XPATH, password_xpath)
-    password_field.send_keys(Keys.ENTER)
-    time.sleep(2)
-    printt(f"Tweakers logged in")
 
 
 def marktplaats_enter_title(title):
@@ -207,21 +213,14 @@ def marktplaats_upload_photos(image_paths):
         center_element(last_upload_element)
         last_upload_element.send_keys(file_path)
         # wait for next upload box to appear
+        count = 0
         while True:
             if len(driver.find_elements(By.XPATH, upload_xpath)) > len(upload_elements):
                 break
             time.sleep(0.1)
-
-
-def tweakers_upload_photos(image_paths):
-    upload_xpath = '//*[@id="row_advertisement_form_images"]/div[2]/twk-multi-image-upload/div[1]/input[1]'
-
-    count = 0
-    # this is fast but will take a while to show up
-    for file_path in image_paths:
-        count += 1
-        printt(f"Uploading photo {count}/{len(image_paths)}: {file_path}")
-        enter_field(upload_xpath, file_path)
+            count += 1
+            if count > 100:
+                break
 
 
 def upload_marktplaats(ad):
@@ -245,9 +244,13 @@ def upload_marktplaats(ad):
     asking_price_xpath = '//*[@id="syi-bidding-price"]/input'
     enter_field(asking_price_xpath, ad['price'])
 
+    # allow bidding
+    bidding_toggle_xpath = '//*[@id="syi-bidding-switch"]'
+    click_button(bidding_toggle_xpath)
+
     # min price
-    min_price_xpath = '//*[@id="syi-bidding-minimumprice"]/input'
-    enter_field(min_price_xpath, ad['min_price'])
+    # min_price_xpath = '//*[@id="syi-bidding-minimumprice"]/input'
+    # enter_field(min_price_xpath, ad['min_price'])
 
     # zip code
     zip_code_xpath = '//*[@id="postCode"]/input'
@@ -255,11 +258,12 @@ def upload_marktplaats(ad):
 
     # delivery method
     delivery_method_xpath = '//*[@id="deliveryMethod"]/div/select'
-    select_dropdown(delivery_method_xpath, "Ophalen")
+    delivery_method_alt_xpath = '//*[@id="Verzenden"]'  # soms veranderd de pagina
+    select_dropdown(delivery_method_xpath, "Verzenden")
+    click_button(delivery_method_alt_xpath)
 
     # advertisement plan
-    plan_xpath = '//*[@id="js-bundle-FREE"]'
-    remove_hidden_attribute(plan_xpath)
+    plan_xpath = '//*[contains(text(), "Standaard zichtbaarheid")]'
     click_button(plan_xpath)
 
     print(f"Verify the details and click upload to place the advertisement to Marktplaats.")
@@ -267,6 +271,37 @@ def upload_marktplaats(ad):
 
     print(f"Uploaded to Marktplaats!")
     time.sleep(delay)
+
+
+########################################################################################################################
+# MARKTPLAATS
+
+# TWEAKERS
+########################################################################################################################
+
+def login_tweakers(username, password):
+    printt(f"Logging in to Tweakers with {username}, {password}")
+    tab_and_get(tweakers_login_url, 2)
+    username_xpath = '//*[@id="tweakers_login_form_user"]'
+    password_xpath = '//*[@id="tweakers_login_form_password"]'
+    enter_field(username_xpath, username)
+    enter_field(password_xpath, password)
+    time.sleep(delay)
+    password_field = driver.find_element(By.XPATH, password_xpath)
+    password_field.send_keys(Keys.ENTER)
+    time.sleep(2)
+    printt(f"Tweakers logged in")
+
+
+def tweakers_upload_photos(image_paths):
+    upload_xpath = '//*[@id="row_advertisement_form_images"]/div[2]/twk-multi-image-upload/div[1]/input[1]'
+
+    count = 0
+    # this is fast but will take a while to show up
+    for file_path in image_paths:
+        count += 1
+        printt(f"Uploading photo {count}/{len(image_paths)}: {file_path}")
+        enter_field(upload_xpath, file_path)
 
 
 def upload_tweakers(ad):
@@ -296,25 +331,25 @@ def upload_tweakers(ad):
     # photos
     tweakers_upload_photos(ad["image_paths"])
 
-    # zip code
-    zip_code_xpath = '//*[@id="advertisement_form_address_postalCode"]'
-    enter_field(zip_code_xpath, ad['zip_code'])
+    # zip code # already entered if one ad was uploaded ever
+    # zip_code_xpath = '//*[@id="advertisement_form_address_postalCode"]'
+    # enter_field(zip_code_xpath, ad['zip_code'])
 
     # delivery cost
     delivery_costs_xpath = '//*[@id="advertisement_form_deliveryCosts"]'
     select_dropdown(delivery_costs_xpath, "Niet van toepassing")
 
     # delivery method
-    delivery_methods_xpath = '//*[@id="advertisement_form_deliveryMethods_1"]'
-    click_button(delivery_methods_xpath)  # checkbox
+    # delivery_methods_xpath = '//*[@id="advertisement_form_deliveryMethods_1"]'
+    # click_button(delivery_methods_xpath)  # checkbox
 
     # payment method
-    payment_method_xpath = '//*[@id="advertisement_form_paymentOptions_7"]'
-    click_button(payment_method_xpath)  # checkbox
+    # payment_method_xpath = '//*[@id="advertisement_form_paymentOptions_7"]'
+    # click_button(payment_method_xpath)  # checkbox
 
     # allow reactions
-    allow_reactions_xpath = '//*[@id="advertisement_form_allowResponses"]'
-    click_button(allow_reactions_xpath)  # checkbox
+    # allow_reactions_xpath = '//*[@id="advertisement_form_allowResponses"]'
+    # click_button(allow_reactions_xpath)  # checkbox
 
     print(f"Verify the details and click upload to place the advertisement to Tweakers.")
     sleep_until_url_change()
@@ -322,62 +357,12 @@ def upload_tweakers(ad):
     print(f"Uploaded to Tweakers!")
 
 
-def assemble_advertisement_info(dir_path):
-    ad = {}
-    # find .txt file
-    txt_files = [os.path.join(dir_path, x) for x in os.listdir(dir_path) if x.endswith(".txt")]
-    if len(txt_files) != 1:
-        quit(f"Expected 1 description .txt file but found {len(txt_files)}")
+########################################################################################################################
+# TWEAKERS
 
-    # read .txt file
-    with open(txt_files[0]) as file:
-        file_lines = file.readlines()
 
-    # title
-    title = file_lines[0].strip()
-    ad["title"] = title
-
-    # price
-    asking_price = file_lines[1].strip()
-    if args.default_price != "":
-        asking_price = args.default_price
-    ad["price"] = asking_price
-
-    # min price
-    min_price = args.minimum_price
-    if min_price == "":
-        min_price = "0"
-    ad["min_price"] = min_price
-
-    # description text
-    description_text = ""
-    for line in file_lines[2:]:
-        description_text += line
-    description_text.strip()
-    description_text += args.default_description  # append default description footer
-    ad["description"] = description_text
-
-    # zip code
-    ad["zip_code"] = zip_code  # from args / file
-
-    # folder path
-    ad["dir_path"] = dir_path
-
-    # image paths
-    image_paths = []
-    for filename in os.listdir(dir_path):
-        file_path = os.path.join(dir_path, filename)
-        if not os.path.isfile(file_path):
-            continue
-        if any(substring in file_path.lower() for substring in (".jpg", ".png", ".jpeg")):
-            image_paths.append(file_path)
-
-    image_paths = natsorted(image_paths, key=lambda y: y.lower())  # sort items same as the file manager does
-    ad["image_paths"] = image_paths
-
-    printt(f"Parsed advertisement: {ad}")
-    return ad
-
+# UTIL
+########################################################################################################################
 
 def ask_folders():
     # returns None if no folder is choosen, otherwise a list of paths
@@ -422,7 +407,7 @@ def get_folder_paths():
 
     if len(folder_paths) == 0:
         quit(f"No folders found at this depth level. Try lowering it.")
-    print(f"Uploading items in folders: {folder_paths}")
+    printt(f"Uploading items in folders: {folder_paths}")
     return folder_paths
 
 
@@ -449,6 +434,94 @@ def read_credential(argument=None, credential_path=None):
     printt(f"Read credential '{credential}' from {credential_path}")
     return credential
 
+
+def get_driver():
+    # instantiate chrome
+    options = uc.ChromeOptions()
+    options.add_experimental_option(
+        "prefs",
+        {
+            "credentials_enable_service": False,
+            "profile.password_manager_enabled": False,
+        },
+    )
+    options.add_argument("--disable-notifications")  # not needed
+    options.add_argument("--disable-popup-blocking")  # disable are you sure to leave this page? does not work
+    options.set_capability('unhandledPromptBehavior', 'accept')  # default is dismiss, which confuses all the code
+
+    driver = uc.Chrome(options=options)
+    return driver
+
+
+########################################################################################################################
+# UTIL
+
+# AD PARSER
+########################################################################################################################
+
+def assemble_advertisement_info(dir_path):
+    ad = {}
+    # find .txt file
+    txt_files = [os.path.join(dir_path, x) for x in os.listdir(dir_path) if x.endswith(".txt")]
+    if len(txt_files) != 1:
+        quit(f"Expected 1 description .txt file but found {len(txt_files)}")
+
+    # read .txt file
+    with open(txt_files[0]) as file:
+        file_lines = file.readlines()
+
+    # title
+    title = file_lines[0].strip()
+    ad["title"] = title
+
+    # price
+    asking_price = file_lines[1].strip()
+    if args.default_price != "":
+        asking_price = args.default_price
+    ad["price"] = asking_price
+
+    # min price
+    min_price = args.minimum_price
+    if min_price == "":
+        min_price = "0"
+    ad["min_price"] = min_price
+
+    # description text
+    description_text = ""
+    for line in file_lines[2:]:
+        description_text += line
+    description_text.strip()
+    default_description = args.default_description.replace("\\n", Keys.ENTER)  # append default description footer
+    description_text += default_description
+    ad["description"] = description_text
+
+    # zip code
+    ad["zip_code"] = zip_code  # from args / file
+
+    # folder path
+    ad["dir_path"] = dir_path
+
+    # image paths
+    image_paths = []
+    for filename in os.listdir(dir_path):
+        file_path = os.path.join(dir_path, filename)
+        if not os.path.isfile(file_path):
+            continue
+        if any(substring in file_path.lower() for substring in (".jpg", ".png", ".jpeg")):
+            image_paths.append(file_path)
+
+    image_paths = natsorted(image_paths, key=lambda y: y.lower())  # sort items same as the file manager does
+    ad["image_paths"] = image_paths
+
+    printt(f"Parsed advertisement: {ad}")
+    return ad
+
+
+########################################################################################################################
+# AD PARSER
+
+# CLI
+########################################################################################################################
 
 parser = argparse.ArgumentParser(
     prog="Advertisement uploader",
@@ -506,7 +579,6 @@ parser.add_argument("folder_paths",
 args = parser.parse_args()
 printt(args)
 
-# load sensitive information from local disk
 marktplaats_username = read_credential(
     credential_path="credentials/marktplaats_username.txt",
     argument=args.marktplaats_username
@@ -529,35 +601,33 @@ zip_code = read_credential(
 )
 
 
-def get_driver():
-    # instantiate chrome
-    options = uc.ChromeOptions()
-    options.add_experimental_option(
-        "prefs",
-        {
-            "credentials_enable_service": False,
-            "profile.password_manager_enabled": False,
-        },
-    )
-    options.add_argument("--disable-notifications")  # not needed
-    options.add_argument("--disable-popup-blocking")  # disable are you sure to leave this page? does not work
-    options.set_capability('unhandledPromptBehavior', 'accept')  # default is dismiss, which confuses all the code
-
-    driver = uc.Chrome(options=options)
-    return driver
+########################################################################################################################
+# CLI
 
 
+# PROGRAM :')
+########################################################################################################################
+
+def upload_ads():
+    folder_paths = get_folder_paths()
+    login_marktplaats(marktplaats_username, marktplaats_password)
+    # login_tweakers(tweakers_username, tweakers_password)
+    for folder_path in folder_paths:
+        advertisement_info = assemble_advertisement_info(folder_path)
+        upload_marktplaats(advertisement_info)
+        # upload_tweakers(advertisement_info)
+
+
+########################################################################################################################
+# PROGRAM :')
+
+
+# PROGRAM WRAPPER
+########################################################################################################################
+driver = get_driver()
 try:
     try:
-        driver = get_driver()
-        folder_paths = get_folder_paths()
-        login_marktplaats(marktplaats_username, marktplaats_password)
-        login_tweakers(tweakers_username, tweakers_password)
-        for folder_path in folder_paths:
-            advertisement_info = assemble_advertisement_info(folder_path)
-            upload_marktplaats(advertisement_info)
-            upload_tweakers(advertisement_info)
-
+        upload_ads()
     except Exception as e:
         traceback.print_exc()
 
@@ -572,3 +642,7 @@ try:
             count = 0
 except KeyboardInterrupt:
     print(f"Program stopped manually.")
+driver.quit()
+quit(0)
+########################################################################################################################
+# PROGRAM WRAPPER
